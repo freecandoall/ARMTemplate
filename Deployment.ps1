40cd2f9c5a109cc0547b45e16cb89c9ee65ef7f2
KyungSu, Han <rapker@pieceton.com>
2019-11-01 15:13:18 +0900

    * PowerShell New-AzDeployment 를 사용하여 템플릿 배포 샘플   - /WebAppSample/Web_vmLinux.json

---
+++ Deployment.ps1
@@ -0,0 +1,16 @@
+﻿$rootPath = if ($psISE) { Split-Path -Path $psISE.CurrentFile.FullPath } else { $global:PSScriptRoot }
+$subPath = "temp01.json";
+
+$fullPath = $rootPath + "\" + $subPath
+
+$iotRoot = $rootPath + "\" + "IotSample\"
+$vmRoot = $rootPath + "\" + "vmSample\"
+$webRoot = $rootPath + "\" + "WebAppSample\"
+
+
+$targetJson = $webRoot + "Web_vmLinux.json"
+$targetJson
+
+#Connect-AzAccount
+#Set-AzContext -SubscriptionId "xxxx-xxxx-xxxx-xxxx"
+New-AzDeployment -Location "koreacentral" -TemplateFile $targetJson
