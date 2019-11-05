68981a356715e32f945bc97d21e370867caba6a5
KyungSu, Han <rapker@pieceton.com>
2019-11-05 15:04:35 +0900

    * 생성은 되지만 Subnets/webapp 과 function이 연결되지 않는 상태

--- Deployment.ps1
+++ Deployment.ps1
@@ -9,8 +9,11 @@ $webRoot = $rootPath + "\" + "WebAppSample\"
 
 
 $targetJson = $webRoot + "Web_vmLinux.json"
+#$targetJson = $webRoot + "Web_vmWindowSql_function.json"
 $targetJson
 
 #Connect-AzAccount
+#Get-AzSubscription
 #Set-AzContext -SubscriptionId "xxxx-xxxx-xxxx-xxxx"
 New-AzDeployment -Location "koreacentral" -TemplateFile $targetJson
+#New-AzDeployment -Location "koreacentral" -TemplateFile $webRoot + "Web_vmWindowSql_function.json"
